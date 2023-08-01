47df075ec2cabb95a599a6a7695b942c191a76d5
SiddharthPenmetsa <135067635+SiddharthPenmetsa@users.noreply.github.com>
2023-08-01 07:07:05 -0400

    Create training.py

---
+++ training.py
@@ -0,0 +1,52 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import torch.nn as nn
+from facenet_pytorch import MTCNN, InceptionResnetV1
+import os
+from PIL import Image, ImageOps
+from torch.utils.data import Dataset
+from torchvision import transforms
+import torch
+import torch.optim as optim
+import preprocessing as pp
+from multiprocessing import Process
+
+def train_model():
+    pp.resnet.train()  # Set the model to training mode
+
+    num_epochs = 10  # You can adjust this value based on your needs
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(pp.resnet.parameters(), lr=0.001)
+
+    for epoch in range(num_epochs):
+        running_loss = 0.0
+        for i, data in enumerate(pp.train_loader, 0):
+            # Get the inputs; data is a list of [inputs, labels]
+            inputs = data
+            print(data.shape)
+            # Zero the parameter gradients
+            optimizer.zero_grad()
+
+            # Forward pass
+            outputs = pp.resnet(inputs)
+            loss = criterion(outputs)
+
+            # Backward pass and optimization=
+            loss.backward()
+            optimizer.step()
+
+            # Print statistics
+            running_loss += loss.item()
+            if i % 100 == 99:  # Print every 100 mini-batches
+                print('[%d, %5d] loss: %.3f' %
+                    (epoch + 1, i + 1, running_loss / 100))
+                running_loss = 0.0
+    
+def worker():
+    train_model()
+if __name__=='__main__':
+    p = Process(target=worker)
+    p.start()
+    p.join()
