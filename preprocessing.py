d8f57a328c5490da1a34a781de19dd0aa1e83ad2
SiddharthPenmetsa <135067635+SiddharthPenmetsa@users.noreply.github.com>
2023-08-05 11:09:37 -0400

    Fixed dimensionality error and created targets for training

--- preprocessing.py
+++ preprocessing.py
@@ -10,13 +10,9 @@ from torchvision import transforms
 import torch
 import torch.optim as optim
 
-image_size=48
-margin=0
-mtcnn = MTCNN(image_size=image_size, margin=margin)
-
 resnet = InceptionResnetV1(pretrained='vggface2', classify=True).eval()
-num_features = resnet.last_linear.in_features
-resnet.last_linear = nn.Linear(num_features, 7)
+num_features = resnet.logits.in_features
+resnet.logits = nn.Linear(num_features, 7)
 
 children = list(resnet.children())
 
@@ -50,13 +46,29 @@ class FaceDataset(Dataset):
 
     def __getitem__(self, idx):
         img_path = self.file_list[idx]
+        folder_path = os.path.dirname(img_path)
+        if folder_path == 'train\\angry' or folder_path == 'test\\angry':
+            label = 0
+        if folder_path == 'train\\disgust' or folder_path == 'test\\angry':
+            label = 1
+        if folder_path == 'train\\fear' or folder_path == 'test\\angry':
+            label = 2
+        if folder_path == 'train\\happy' or folder_path == 'test\\angry':
+            label = 3
+        if folder_path == 'train\\neutral' or folder_path == 'test\\angry':
+            label = 4
+        if folder_path == 'train\\sad' or folder_path == 'test\\angry':
+            label = 5
+        if folder_path == 'train\\surprise' or folder_path == 'test\\angry':
+            label = 6
         img = Image.open(img_path)
         img_resized = img.resize((224, 224))
         img_color = ImageOps.colorize(img_resized, "black", "white")
-        img_cropped = mtcnn(img_color)
         if self.transform:
             img_tensor = self.transform(img_color)
-        return img_tensor
+        tensor1 = img_tensor.cpu()
+        img_tensor = tensor1.numpy()
+        return img_tensor, label
 
 train_dataset = FaceDataset(dataset_path, transform=transform)
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=10)
