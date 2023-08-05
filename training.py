4f641eb25c813a5aa6ef22b45adb2cdd0c87e616
SiddharthPenmetsa <135067635+SiddharthPenmetsa@users.noreply.github.com>
2023-08-05 11:10:23 -0400

    Redid the training loop to account for new labels

--- training.py
+++ training.py
@@ -13,33 +13,41 @@ import preprocessing as pp
 from multiprocessing import Process
 
 def train_model():
-    pp.resnet.train()  
-
-    num_epochs = 10  
-
+    print('yes')
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(pp.resnet.parameters(), lr=0.001)
 
+    # Move the model to GPU if available
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    pp.resnet.to(device)
+
+    # Number of epochs
+    num_epochs = 10
+
+    # Training loop
     for epoch in range(num_epochs):
+        pp.resnet.train()  # Set the model to training mode
         running_loss = 0.0
-        for i, data in enumerate(pp.train_loader, 0):
-            
-            inputs = data
-            print(data.shape)
-            
+        print(0)
+        for images, labels in pp.train_loader:
+            images, labels = images.to(device), labels.to(device)
+
+            # Zero the parameter gradients
             optimizer.zero_grad()
+            print(1)
+            # Forward pass
+            outputs = pp.resnet(images)
+            loss = criterion(outputs, labels)
 
-            outputs = pp.resnet(inputs)
-            loss = criterion(outputs)
-
+            # Backward pass and optimization
             loss.backward()
             optimizer.step()
 
-            running_loss += loss.item()
-            if i % 100 == 99:  
-                print('[%d, %5d] loss: %.3f' %
-                    (epoch + 1, i + 1, running_loss / 100))
-                running_loss = 0.0
+            running_loss += loss.item() * images.size(0)
+
+        # Print epoch loss
+        epoch_loss = running_loss / len(pp.train_dataset)
+        print(f'Epoch {epoch+1}/{num_epochs}, Loss: {epoch_loss:.4f}')
     
 def worker():
     train_model()
