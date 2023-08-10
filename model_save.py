b9ee88439e735724b0d1e9ab2fb1e9438ab007e1
SiddharthPenmetsa <135067635+SiddharthPenmetsa@users.noreply.github.com>
2023-08-10 12:19:13 -0400

    made code for creating json file

--- model_save.py
+++ model_save.py
@@ -8,13 +8,21 @@ import tensorflow as tf
 model = torch.load('trained_model.pt')
 
 torch.onnx.export(model,               
-                  torch.randn(1, 3, 224, 224), # dummy input (required)
-                  "resnet18.onnx",   # where to save the model (can be a file or file-like object)
-                  export_params=True) # store the trained parameter weights inside the model file
+                  torch.randn(1, 3, 224, 224), 
+                  "trained_onnx_model",
+                  export_params=True)
 
-model = onnx.load("train_onnx_model")
+model = onnx.load("trained_onnx_model")
 onnx.checker.check_model(model)
 
 tf_model = onnx_tf.convert_from_onnx(model)
+tf.io.write_graph(tf_model, ".", "trained_onnx_model", as_text=False)
 
-tf.io.write_graph(tf_model, ".", "train_onnx_model", as_text=False)
+model_json = tf_model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+
+with open("model.json", "r") as json_file:
+    loaded_model_json = json_file.read()
+
+loaded_model = tf.keras.models.model_from_json(loaded_model_json)
